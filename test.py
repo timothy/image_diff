bd68a879652fe7fdd5d0b8108836a0eefd2c7333
timothy <t192.168.1.0@gmail.com>
2020-05-14 22:20:30 -0700

    Initial commit

---
+++ test.py
@@ -0,0 +1,19 @@
+from PIL import Image, ImageChops
+
+
+img1 = Image.open('./1.png')
+img2 = Image.open('./2.png')
+
+img3 = Image.open('./A.jpg')
+img4 = Image.open('./B.jpg')
+diff1 = ImageChops.difference(img3, img4)
+diff = ImageChops.difference(img2, img1)
+
+print(diff.getbbox())
+if diff.getbbox():
+    diff.show()
+
+print(diff1.getbbox())
+
+if diff1.getbbox():
+    diff1.show()

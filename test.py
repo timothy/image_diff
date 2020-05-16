b4aeb68b8be641d50ee37ee0f5593f3f5af3d21d
timothy <t192.168.1.0@gmail.com>
2020-05-16 10:47:16 -0700

    Fixed image diff problem: https://stackoverflow.com/a/61815268/5398884

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from PIL import Image, ImageChops
 
 
-img1 = Image.open('./1.png')
-img2 = Image.open('./2.png')
+img1 = Image.open('./1.png').convert('RGB')
+img2 = Image.open('./2.png').convert('RGB')
 
 img3 = Image.open('./A.jpg')
 img4 = Image.open('./B.jpg')
